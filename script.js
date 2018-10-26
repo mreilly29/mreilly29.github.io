9905043524abe1044a70b804a6849a7492548b6a
mreilly29 <mreilly18@jcu.edu>
2018-10-26 19:15:51 -0400

    Added Skills section to a carousel

---
+++ script.js
@@ -0,0 +1,68 @@
+(function() {
+
+    setUpPage();
+  
+    function setUpPage() {
+        TechInventoryCarousel();
+    }
+  // Image Carousel / Slider
+  function TechInventoryCarousel() {
+
+    const sliderPrev = document.getElementById('slider-prev');
+    const sliderNext = document.getElementById('slider-next');
+    const sliderFrame = document.getElementById('slider-frame');
+    const sliderCaption = document.getElementById('slider-caption');
+
+    const slides =
+      Array.from(sliderFrame.querySelectorAll('div[data-src]'))
+        .map(div => {
+          return {
+            imageUrl: div.getAttribute('data-src'),
+            caption: div.textContent.trim(),
+          };
+        });
+
+    let sliderIndex = 0;
+    displaySliderImage();
+
+    function displaySliderImage() {
+      let { imageUrl, caption } = slides[sliderIndex];
+      sliderFrame.style.backgroundImage = `url('${imageUrl}')`;
+      let count = `(${sliderIndex+1}/${slides.length}) `;
+      sliderCaption.innerHTML = count + caption;
+      sliderCaption.classList.add('flash');
+      setTimeout(() => {
+        sliderCaption.classList.remove('flash');
+      }, 0);
+    }
+
+    function displayPrevSliderImage() {
+      sliderIndex--;
+      if (sliderIndex < 0) {
+        sliderIndex = slides.length - 1;
+      }
+      displaySliderImage();
+    }
+
+    function displayNextSliderImage() {
+      sliderIndex++;
+      if (sliderIndex === slides.length) {
+        sliderIndex = 0;
+      }
+      displaySliderImage();
+    }
+
+    sliderPrev.addEventListener('click', displayPrevSliderImage);
+    sliderNext.addEventListener('click', displayNextSliderImage);
+
+    setInterval(function() {
+      displayNextSliderImage();
+    }, 4000);
+  }
+
+
+
+
+
+
+})();
